627b9f20334992febd8010d933d40af7e0b10524
John Pavel <jrp@dial.pipex.com>
2018-11-05 23:49:01 +0000

    Missed prettification

--- src/Data/Zipper/Direction.hs
+++ src/Data/Zipper/Direction.hs
@@ -1,32 +1,30 @@
+module Data.Zipper.Direction where
 
-module Data.Zipper.Direction
-  where
-
-import Text.ParserCombinators.Parsec
 import Hnt.Utils.Parser
+import Text.ParserCombinators.Parsec
 
-
-data Dir dir = Up | Down | DownTo dir | Next
-  deriving (Eq,Ord,Show,Read)
+data Dir dir
+  = Up
+  | Down
+  | DownTo dir
+  | Next
+  deriving (Eq, Ord, Show, Read)
 
 parse'dir :: GenParser Char st a -> GenParser Char st (Dir a)
-parse'dir p = (    (string "up"       >> return Up)
-               <|> (string "next"     >> return Next)
-               <|> (string "down"     >> (    try (do spaces >> string "to" >> spaces
-                                                      DownTo <$> p
-                                                   )
-                                          <|>  return Down
-                                         )
-                   )
-              ) <?> "up, down, down to, next"
-
+parse'dir p =
+  ((string "up" >> return Up) <|> (string "next" >> return Next) <|>
+   (string "down" >>
+    (try
+       (do spaces >> string "to" >> spaces
+           DownTo <$> p) <|>
+     return Down))) <?>
+  "up, down, down to, next"
 
 instance (ParsecRead a) => ParsecRead (Dir a) where
   parsecRead = parse'dir parsecRead
 
-
 instance Functor Dir where
- fmap f (DownTo  d) = DownTo (f d)
- fmap _          Up = Up
- fmap _        Next = Next
- fmap _        Down = Down
+  fmap f (DownTo d) = DownTo (f d)
+  fmap _ Up = Up
+  fmap _ Next = Next
+  fmap _ Down = Down
--- src/Data/Zipper/Zipper.hs
+++ src/Data/Zipper/Zipper.hs
@@ -1,40 +1,36 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+
 -- | This is the Zipper Module inspired by
 -- | /Generic Zipper and its applications/ by Oleg Kiselyov and al.
 -- | Available at <http://okmij.org/ftp/Computation/Continuations.html#zipper>
+module Data.Zipper.Zipper where
 
-module Data.Zipper.Zipper
-  where
 import Data.Zipper.Direction
 
 import Control.Monad
 import Control.Monad.ContState
 
 -- | Requests that can be sent to the zipper
-data ZipReq term dir = Move (Dir dir) -- ^ Move the zipper to dir
-                     | Update term   -- ^ Update (replace) the current subterm by term
+data ZipReq term dir
+  = Move (Dir dir) -- ^ Move the zipper to dir
+  | Update term -- ^ Update (replace) the current subterm by term
 
 -- | The Zipper Data Structure
-data Zipper term dir m = Zipper { -- | The Path of the current subterm
-                                  zip'path :: [dir]  ,
+data Zipper term dir m = Zipper -- | The Path of the current subterm
+  { zip'path :: [dir]
                                   -- | The current subterm
-                                  zip'term :: term ,
+  , zip'term :: term
                                   -- | The continuation. It takes a request and give another zipper
-                                  zip'cont :: ZipReq term dir -> m (Zipper term dir m)
-                                }
-
+  , zip'cont :: ZipReq term dir -> m (Zipper term dir m)
+  }
 
 -- | Show a path ala Unix, the list is reversed !
 showPath :: (Show a) => [a] -> String
-showPath l = "/" ++ concatMap (\ d -> "/" ++ show d) (reverse l)
+showPath l = "/" ++ concatMap (\d -> "/" ++ show d) (reverse l)
 
 instance (Show dir, Show term) => Show (Zipper term dir m) where
-  show (Zipper p t _) =        "Zipper "
-                      ++     " (term = " ++ show t
-                      ++    ") (path = " ++ showPath p
-                      ++    ")"
-
-
+  show (Zipper p t _) =
+    "Zipper " ++ " (term = " ++ show t ++ ") (path = " ++ showPath p ++ ")"
 
 -- | Updates the current subterm of a zipper
 zip'update :: (Monad m) => term -> Zipper term dir m -> m (Zipper term dir m)
@@ -49,20 +45,21 @@ zip'move :: (Monad m) => Dir dir -> Zipper term dir m -> m (Zipper term dir m)
 zip'move d z = zip'cont z (Move d)
 
 -- | makes a number of moves
-zip'moves :: (Monad m) => [Dir dir] -> Zipper term dir m -> m (Zipper term dir m)
+zip'moves ::
+     (Monad m) => [Dir dir] -> Zipper term dir m -> m (Zipper term dir m)
 zip'moves = flip $ foldM $ flip zip'move
 
 -- | moves to the root
 zip'toRoot :: (Monad m) => Zipper term dir m -> m (Zipper term dir m)
-zip'toRoot z = if zip'isRoot z
-                then return z
-                else zip'move Up z >>= zip'toRoot
-
-
+zip'toRoot z =
+  if zip'isRoot z
+    then return z
+    else zip'move Up z >>= zip'toRoot
 
 -- | The class of term that supports this zipper
 -- | The minimal implementation is possibleDirs
-class (Eq dir) => ZipperAble term dir where
+class (Eq dir) =>
+      ZipperAble term dir
   -- | give all possibles branch to the current term
   -- | possibleDirs term = [(dir, (subterm , rebuild'function)) , ....]
   -- | such that subterm is the subterm of term on the direction dir
@@ -73,62 +70,66 @@ class (Eq dir) => ZipperAble term dir where
   -- |                                            (PairRight , (u , (\x -> Pair  s x)))
   -- |                                          ]
   -- |               possibleDirs (Modal m s) = [ (ModalDown , (s , (\x -> Modal m x))) ]
-  possibleDirs :: term -> [(dir , (term , term -> term))]
-
-
+  where
+  possibleDirs :: term -> [(dir, (term, term -> term))]
 -- Layers : Cont
 --        : Stack of Cmds
 --        : Tail of Stack of Possdirs
 --        : Head of Stack of Possdir
   -- | builds the zipper of a term
   zipper :: term -> Zipper term dir (CS r l m)
-  zipper t = Zipper [] t (\req -> do z <- runContL $ runStackL $ runStackL $ runStackL (
-                                           fix (loop []) t
-                                         )
-                                     zip'cont z req
-                         )
-    where fix f s = do putL $ possibleDirs s
-                       inc  $ putL []
-                       inc2 $ putL []
-                       f s >>= fix f
-
-          loop p s = do req <- getReq p s
-                        case req of
-                         Update u        -> putL (possibleDirs u) >> loop p u
-                         Move  Up        -> return s
-                         Move  Down      -> findOneDown (possibleDirs s) >> loop p s
-                         Move (DownTo d) -> goIn d p s
-                         Move  Next      -> do possdir <- getL
-                                               oldposs <- inc getL
-                                               findNext (possdir : oldposs)
-                                               loop p s
-
-          goIn d p u = case lookup d (possibleDirs u) of
-                         Nothing    -> return u
-                         Just (s,f) -> do newpossdir <- getsL $ filter (\(x,_) -> x /= d)
-                                          oldstack   <- inc getL
-                                          inc  $ pushStateL newpossdir
-                                          putL $ possibleDirs s
-                                          u' <- loop (d : p) s
-                                          putL newpossdir
-                                          inc  $ putL oldstack
-                                          loop p $ f u'
-
-          getReq p s = do x <- inc2 popStateL
-                          case x of
-                           Nothing  -> inc3 $ shiftL (return . Zipper p s)
-                           Just   d -> return (Move d)
-
-
-          findOneDown []          = return False
-          findOneDown ((d,_) : _) = do inc2 $ appendStateL (DownTo d)
-                                       return True
-
-          findNext  []     = return False
-          findNext (x : l) = do b <- findOneDown x
-                                if b
-                                 then return True
-                                 else do inc2 $ appendStateL Up
-                                         findNext l
-
-          runStackL m = fst <$> runStateL [] m
+  zipper t =
+    Zipper
+      []
+      t
+      (\req -> do
+         z <- runContL $ runStackL $ runStackL $ runStackL (fix (loop []) t)
+         zip'cont z req)
+    where
+      fix f s = do
+        putL $ possibleDirs s
+        inc $ putL []
+        inc2 $ putL []
+        f s >>= fix f
+      loop p s = do
+        req <- getReq p s
+        case req of
+          Update u -> putL (possibleDirs u) >> loop p u
+          Move Up -> return s
+          Move Down -> findOneDown (possibleDirs s) >> loop p s
+          Move (DownTo d) -> goIn d p s
+          Move Next -> do
+            possdir <- getL
+            oldposs <- inc getL
+            findNext (possdir : oldposs)
+            loop p s
+      goIn d p u =
+        case lookup d (possibleDirs u) of
+          Nothing -> return u
+          Just (s, f) -> do
+            newpossdir <- getsL $ filter (\(x, _) -> x /= d)
+            oldstack <- inc getL
+            inc $ pushStateL newpossdir
+            putL $ possibleDirs s
+            u' <- loop (d : p) s
+            putL newpossdir
+            inc $ putL oldstack
+            loop p $ f u'
+      getReq p s = do
+        x <- inc2 popStateL
+        case x of
+          Nothing -> inc3 $ shiftL (return . Zipper p s)
+          Just d -> return (Move d)
+      findOneDown [] = return False
+      findOneDown ((d, _):_) = do
+        inc2 $ appendStateL (DownTo d)
+        return True
+      findNext [] = return False
+      findNext (x:l) = do
+        b <- findOneDown x
+        if b
+          then return True
+          else do
+            inc2 $ appendStateL Up
+            findNext l
+      runStackL m = fst <$> runStateL [] m
